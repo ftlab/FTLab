f1184431eb2cb25049cf9dc6fb421d1d2c44cfcc
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-10-16 21:45:56 +0300

    db command

--- FTDb/DbCommandExtension.cs
+++ FTDb/DbCommandExtension.cs
@@ -28,11 +28,45 @@ namespace FTDb
             return cmd;
         }
 
+        /// <summary>
+        /// Установить текст команды
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="commandText"></param>
+        /// <returns></returns>
         public static DbCommand SetCommandText(this DbCommand cmd
             , string commandText)
         {
             if (cmd == null) throw new ArgumentNullException(nameof(cmd));
             cmd.CommandText = commandText;
+            return cmd;
+        }
+
+        /// <summary>
+        /// Установить тип команды
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static DbCommand SetCommandType(this DbCommand cmd
+            , CommandType type)
+        {
+            if (cmd == null) throw new ArgumentNullException(nameof(cmd));
+            cmd.CommandType = type;
+            return cmd;
+        }
+
+        /// <summary>
+        /// Установить таймаут команды
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public static DbCommand SetCommandTimeout(this DbCommand cmd
+            , TimeSpan timeout)
+        {
+            if (cmd == null) throw new ArgumentNullException(nameof(cmd));
+            cmd.CommandTimeout = (int)timeout.TotalSeconds;
             return cmd;
         }
     }
